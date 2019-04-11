6430b2c48f84eadf9b0afe5cab0021b220870c59
joker <510060289@qq.com>
2019-04-11 17:36:30 +0800

    添加数量加减

--- src/models/products/products.js
+++ src/models/products/products.js
@@ -7,13 +7,28 @@ export default {
       { name: '3', id: 3, key: 3 },
       { name: '4', id: 4, key: 4 },
       { name: '5', id: 5, key: 5 },
-    ]
+    ],
+    count: 6
   },
   reducers: {
     'delete'(state, action) {
       const list = state.list.filter(item => item.id !== action.payload.id)
       return {...state, list}
     },
+    'reduce'(state){
+      const count = state.count - 1
+      return {...state, count}
+    },
+    'add'(state){
+      const count = state.count + 1
+      return {...state, count}
+    }
   },
+  // effects: {
+  //   *addAfter1Second(action, { call, put }) {
+  //     yield call(delay, 1000);
+  //     yield put({ type: 'add' });
+  //   },
+  // },
   
 };
--- src/routes/products/products.js
+++ src/routes/products/products.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { connect } from 'dva';
+import { Button } from 'antd';
+
 import ProductList from '../../components/productList/ProductList';
 
-const Products = ({ dispatch, products }) => {
+const Products = ({ dispatch, products, count }) => {
   function handleDelete(id) {
     dispatch({
       type: 'products/delete',
@@ -11,10 +13,27 @@ const Products = ({ dispatch, products }) => {
       },
     });
   }
+
+  function handleReduce(){
+    dispatch({
+      type: 'products/reduce'
+    });
+  }
+
+  function handleAdd(){
+    dispatch({
+      type: 'products/add'
+    });
+  }
+  
   return (
     <div>
       <h2>List of Products</h2>
       <ProductList onDelete={handleDelete} products={products} />
+      <div>
+        <Button onClick={handleReduce}>-</Button>{count}<Button onClick={handleAdd}>+</Button>
+      </div>
+        
     </div>
   );
 };
@@ -22,7 +41,8 @@ const Products = ({ dispatch, products }) => {
 // export default Products;
 function mapStateToProps(state){
   return{
-    products: state.products.list
+    products: state.products.list,
+    count: state.products.count
   }
 }
 export default connect(mapStateToProps)(Products);
